ef54608d3c7968b3b2f85c737032038fd8925ff0
Chris <ba8_100006230602_Chris@git.cloud.tencent.com>
2018-11-04 16:00:55 +0800

    add gateway

---
+++ mydolphin-gateway/src/main/java/cn/mydolphin/gateway/MydolphinGatewayApplication.java
@@ -0,0 +1,12 @@
+package cn.mydolphin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydolphinGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydolphinGatewayApplication.class, args);
+    }
+}
