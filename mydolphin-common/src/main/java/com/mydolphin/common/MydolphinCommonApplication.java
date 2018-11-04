3e83a98ddc60beaeb833e1b164c58c6ffa4ae12e
Chris <ba8_100006230602_Chris@git.cloud.tencent.com>
2018-11-04 15:51:01 +0800

    Initial commit

---
+++ mydolphin-common/src/main/java/com/mydolphin/common/MydolphinCommonApplication.java
@@ -0,0 +1,12 @@
+package com.mydolphin.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydolphinCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydolphinCommonApplication.class, args);
+    }
+}
