19f029a28f8d30deaba9f6fd55e52d9e78f071d0
Chris <ba8_100006230602_Chris@git.cloud.tencent.com>
2019-03-31 18:02:19 +0800

    add mydolpin-user

---
+++ mydolphin-user/src/main/java/cn/mydolphin/mydolphinuser/MydolphinUserApplication.java
@@ -0,0 +1,13 @@
+package cn.mydolphin.mydolphinuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydolphinUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydolphinUserApplication.class, args);
+    }
+
+}
