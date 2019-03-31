ad1b8807180194bf4f7590fe03d1687763d23b4c
Chris <ba8_100006230602_Chris@git.cloud.tencent.com>
2019-03-31 17:55:11 +0800

    add gateway moudle

--- mydolphin-gateway/src/main/java/cn/mydolphin/gateway/MydolphinGatewayApplication.java
+++ mydolphin-gateway/src/main/java/cn/mydolphin/gateway/MydolphinGatewayApplication.java
@@ -1,9 +1,11 @@
 package cn.mydolphin.gateway;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubboConfiguration
 public class MydolphinGatewayApplication {
 
     public static void main(String[] args) {
